04fd64d354cc6ff13e93a77ee458590fe704b055
Damian <dbdamix@gmail.com>
2020-07-13 18:07:01 -0300

    login

---
+++ Data/conn.php
@@ -0,0 +1,10 @@
+
+<?php
+// Connection variables
+$dbhost	= "localhost";	   // localhost or IP
+$dbuser	= "root";		  // database username
+$dbpass	= "root";		     // database password
+$dbname	= "";    // database name
+
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+?>
